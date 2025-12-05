688462d9bc085095eeae24b2638f213cfa149a04
JerenHicks <jeren.hicks@aviture.us.com>
2025-12-04 21:10:38 -0600

    Adding a screen to do backgrounds, incomplete.

---
+++ Assets/Scripts/UI Controllers/PartnerBackstoryPanelUiController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PartnerBackstoryPanelUIController : MonoBehaviour
+{
+    [Header("Input Fields")]
+    [SerializeField] private GameObject panelReference;
+
+    private string selectedPronouns = "He / Him"; // default
+
+    // Called by the pronoun buttons
+
+
+    // Called by the Next button
+    public void OnNextClicked()
+    {
+
+    }
+}
